26a208370246b003b8efafe80f39b55515a18ce3
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-11 11:07:23 -0500

    Update aws_Cli_v2_Install.sh

--- aws_Cli_v2_Install.sh
+++ aws_Cli_v2_Install.sh
@@ -2,6 +2,6 @@
 
 # AWS Cli install for x84_64-bit Linux. Note: It is recommended uninstalling awscli if previous versions installed prior to installing.
 
-$ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
