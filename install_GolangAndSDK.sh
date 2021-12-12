9c31ae8b61996bb8cd02d6ca3e97a2cb72e393e1
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-12 18:05:26 -0500

    Update install_GolangAndSDK.sh

--- install_GolangAndSDK.sh
+++ install_GolangAndSDK.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install Golang 1.17.5 and the AWS SDK for Go 
-wget "http://go.dev/d1/go1.17.5.linux-amd64.tar.gz" -o "go1.17.5.linux-amd64.tar" 
-#     ^check https://go.dev/doc/install for latest version(s)^
-tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+sudo yum install -y golang
+sudo go get -u github.com/aws/aws-sdk-go
+
+# Add "export PATH=$PATH:/usr/local/go/bin" to your environment variable or add this line unto /etc/profile 
