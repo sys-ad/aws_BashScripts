89912f12b2421d7c42b59b98970d9c534a238833
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-15 15:24:19 -0500

    Create createEKS_Cluster.sh

---
+++ createEKS_Cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum update -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/bin --install-dir /usr/bin/aws-cli --update
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:
+
