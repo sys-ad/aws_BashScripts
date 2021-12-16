b1076dff2eaeabeb5348e6cae75628383a96d25a
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-16 09:15:27 -0500

    Update createEKS_Cluster.sh

--- createEKS_Cluster.sh
+++ createEKS_Cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Updates yum packets; updates 
 sudo yum update -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -14,6 +15,9 @@ sudo mv /tmp/eksctl /usr/bin
 
 # --- Provisioning an EKS Cluster using 3 worker nodes.
 
-eksctl create cluster --name dev --version 1.16 --region us-east-1 --nodegroup-name standard-workers --node-type t3.micro --nodes 3 --nodes-min 1 --nodes-max 4 --managed
+eksctl create cluster --name dev --version 1.21 --region us-east-1 --nodegroup-name standard-workers --node-type t3.micro --nodes 3 --nodes-min 1 --nodes-max 4 --managed
 
 # --- From here, use Cloud Formation to view events/logs. You may also navigate EKS control plane for a visual abstration. 
+# --- The --version option requires a compatible client and server nodes. From 12/15/2021 the Kubectl version is 1.21
+# --- The --nodes option creates 3 nodes
+# --- use IAM user with AdministrativeAcess IAM Roles
