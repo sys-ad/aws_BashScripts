7566e8b063672cdd5745dc26c1fca5cf2431bee8
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-26 00:19:56 -0500

    Update create_RouteTable.sh

--- create_RouteTable.sh
+++ create_RouteTable.sh
@@ -6,7 +6,6 @@ echo -n "Enter VPC ID: "
 read VPC_ID
 VPC_ID = $VPC_ID
 echo -n "Give route table name: "
-read route_name
-route_name = $ROUTE_NAME
+read ROUTE_NAME
 aws ec2 create-route-table --vpc-id $VPC_ID \
---tag-specifications 'ResourceType=route-table,Tags={Key=Name,Value=$ROUTE_NAME}]' --output text --query RouteTable.RouteTableId)
+--tag-specifications "ResourceType=route-table,Tags={Key=Name,Value=$ROUTE_NAME}]" --output text --query RouteTable.RouteTableId)
