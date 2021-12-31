a350219b5c90119e17d720275e13c640ad15de70
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-30 19:49:46 -0500

    Create create_IGW_AttachVPC.sh

---
+++ create_IGW_AttachVPC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create internet gateway (IGW) and attach it to an existing VPC,
+
+echo -n "Create IGW; Give it a name: "
+read IGW
+
+INET_GATEWAY_ID=$(aws ec2 create-internet-gateway \
+        --tag-specifiatiions "ResourceType=internet-gateway,Tags=[{Key=Name,Value='$IGW'}]"
+        --output text --query InternetGateway.InternetGatewayId)
+        
+if [ '$?' == 0 ]
+then
+  echo -n "Attach IGW to existing VPC, enter VPC-Id: "
+  read VPC_ID
+  aws ec2 attach-internet-gateway \
+  --internet-gateway-id $INET_GATEWAY_ID --vpc-id $VPC_ID
+else
+  echo -n "Failed to create IGW"
+fi
+
+exit
