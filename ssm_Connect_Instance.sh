ead20646fed8d66c5e0331837cbc39bf616f3918
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-05 23:30:48 -0500

    Update ssm_Connect_Instance.sh

--- ssm_Connect_Instance.sh
+++ ssm_Connect_Instance.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Use SSM to connect to Instance
+
+echo -n "Enter valid Instance ID: "    
+read INSTANCE_ID
+
+aws ssm start-session --target $INSTANCE_ID    # Use Systems Session Manager to connect to an instance through CLI
