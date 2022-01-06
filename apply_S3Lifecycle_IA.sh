002bac3eb73bd489b605e36c688953141b9f34fb
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-06 10:00:22 -0500

    Update apply_S3Lifecycle_IA.sh

--- apply_S3Lifecycle_IA.sh
+++ apply_S3Lifecycle_IA.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Applies Lifecycle rule configuration (json)
-
+echo -n "Enter existing bucket name: "
+read NAME
 echo -n "Enter path of policy file: "
 read PATH
 
 aws s3api put-bucket-lifecycle-configuration \
-    --bucket 
+    --bucket $NAME \
+    --lifecycle-configuration $PATH
