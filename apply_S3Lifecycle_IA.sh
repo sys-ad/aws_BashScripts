49d0d96a68207c7461b16aa2eff6b7a5b4d483e1
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-06 23:48:28 -0500

    Update apply_S3Lifecycle_IA.sh

--- apply_S3Lifecycle_IA.sh
+++ apply_S3Lifecycle_IA.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Applies Lifecycle rule configuration (json)
+# Given the Path to localized configuration (json) policy file, this script applies an S3 
+# lifecycle policy onto an existing bucket.
+# usage example: ./apply_S3lifecycle_IA.sh <file://nameofpolicy-rule.json>
+# Note that the policy should be in a valid schema for the s3api to acknowledge and apply it.
+
 echo -n "Enter existing bucket name: "
 read NAME
-echo -n "Enter path of policy file: "
-read PATH
+aws s3api put-bucket-lifecycle-configuration --bucket $NAME --lifecycle-configuration "$1"
 
-aws s3api put-bucket-lifecycle-configuration --bucket $NAME --lifecycle-configuration $PATH
+
