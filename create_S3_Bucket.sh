ae114e2330f8c82aedbaaa4fb919eadf67533db4
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-09 11:21:51 -0500

    Update create_S3_Bucket.sh

--- create_S3_Bucket.sh
+++ create_S3_Bucket.sh
@@ -5,5 +5,6 @@
 echo -n "Enter bucket name to create bucket(must be unique): "
 read BUCKET_NAME
 
-aws s3api create-bucket -bucket $BUCKET_NAME --region us-east-1    # Regions outside of us-east-1 require the appropriate LocationConstraint to be specified for desired region
+aws s3api create-bucket -bucket $BUCKET_NAME --region us-east-1    # Regions outside of us-east-1 require the 
+                                                                   # appropriate LocationConstraint to be specified for desired region
 
