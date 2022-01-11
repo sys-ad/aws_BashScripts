38ba3fbfdc83e149217cad5e7bbbb0857869de28
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-11 13:31:49 -0500

    Update create_SG_Attach_Ec2.sh

--- create_SG_Attach_Ec2.sh
+++ create_SG_Attach_Ec2.sh
@@ -13,4 +13,12 @@ SG_ID=$(aws ec2 create-security-group \
     --vpc-id $VPC_ID \
     output text --query GroupId)
     
+echo -n "Enter valid instance-id: "
+read INSTANCE_ID
+
+# Attach security group to instance
+
+aws ec2 modify-instance-attribute --instance-id $INSTANCE_ID \
+    --groups $SG_ID
+    
  
