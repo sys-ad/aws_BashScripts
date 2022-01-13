ca860da856e8417ea699482682a314998dc25f11
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-12 20:53:12 -0500

    Update add_SG_IngressRule_TCP.sh

--- add_SG_IngressRule_TCP.sh
+++ add_SG_IngressRule_TCP.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# adds an ingress rule to a given security group at allows access on TCP port 22
+# adds an ingress rule to a given security group at allows access on TCP port 22 from itself
+
+echo -n "Enter valid sg-id: "    # Enter a valid security group id
+read SG_ID
+
+aws ec2 authorize-security-group-ingress \
+        --group-id $SG_ID \
+        --source-group $SG_ID \
+        --protocol tcp \
+        --port 22 \
