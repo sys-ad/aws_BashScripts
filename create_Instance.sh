9cf89c59407c80894fc2618ab97922d7076222dc
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-14 23:34:09 -0500

    Update create_Instance.sh

--- create_Instance.sh
+++ create_Instance.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Creates running instance
+
 aws ec2 run-instances --key-name KEYNAME --instance-type t3.micro --image-id AMIID \
 --user-data file:///home/ec2-user/UserData.txt 
 --security-group-ids HTTPACCESS 
