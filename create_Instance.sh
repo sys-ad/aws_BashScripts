8c486aaa205f37dd958dde8ec8769ab0e95ecc9f
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-14 23:32:58 -0500

    Update create_Instance.sh

--- create_Instance.sh
+++ create_Instance.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+aws ec2 run-instances --key-name KEYNAME --instance-type t3.micro --image-id AMIID \
+--user-data file:///home/ec2-user/UserData.txt 
+--security-group-ids HTTPACCESS 
+--subnet-id SUBNETID 
+--associate-public-ip-address 
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WebServerBaseImage}]' 
+--output text --query 'Instances[*].InstanceId'
 
