1c2c95c8abcd6c4a5ee78479ac00ac2b031cf79b
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-24 22:56:08 -0500

    Update analyze_VPC_Reachability.sh

--- analyze_VPC_Reachability.sh
+++ analyze_VPC_Reachability.sh
@@ -7,3 +7,7 @@ read INSTANCE_ID1
 echo "Enter destination (valid instance-id): "
 read INSTANCE_ID2
 
+INSIGHTS_PATH_ID=$(aws ec2 create-network-insights-path \
+    --source $INSTANCE_ID1 --destination-port 22 \
+    --destination $INSTANCE_ID2 --protocol tcp \
+    --output text --query NetworkInsightsPath.NetworkInsightsPathId)
