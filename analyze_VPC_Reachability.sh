b1ce600037e22b5618eb556f397a1a34bb0620a8
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-24 22:58:10 -0500

    Update analyze_VPC_Reachability.sh

--- analyze_VPC_Reachability.sh
+++ analyze_VPC_Reachability.sh
@@ -2,12 +2,16 @@
 
 # Tests access across subnets for connectivity (SSH)
 
-echo "Creating network insights path... enter source (valid instance-id): "
+echo -n "Creating network insights path... enter source (valid instance-id): "
 read INSTANCE_ID1
-echo "Enter destination (valid instance-id): "
+echo -n "Enter destination (valid instance-id): "
 read INSTANCE_ID2
 
 INSIGHTS_PATH_ID=$(aws ec2 create-network-insights-path \
     --source $INSTANCE_ID1 --destination-port 22 \
     --destination $INSTANCE_ID2 --protocol tcp \
     --output text --query NetworkInsightsPath.NetworkInsightsPathId)
+
+# Implement a wait response here..
+
+echo -n "Starting ingsights analysis of instances..."
