071d24c56535d3674dc6adf1f25a7b2a7f2d76c8
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-26 04:19:53 -0500

    Update create_NAT_EIP_Route.sh

--- create_NAT_EIP_Route.sh
+++ create_NAT_EIP_Route.sh
@@ -2,19 +2,19 @@
 
 # Creates an Elastic IP, NAT Gateway, and route in existing route tables.
 
-echo "Create EIP to be used with NAT gateway: "
-sleep 10
-ALLOCATION_ID=$(aws ec2 allocate-address --domain vpc \
-                -output text --query AllocationId)
-                
+echo -n "Creating EIP to be used with NAT gateway... "
 sleep 5
-                
+ALLOCATION_ID=$(aws ec2 allocate-address --domain vpc \
+                --output text --query AllocationId)
+
+sleep 5
+
 if [ $? == 0 ]
 then
-    echo -n "Successfully created EIP"
+    echo "Successfully created EIP"
     sleep 5
 else
-    echo -n "Could not create EIP"
+    echo "Could not create EIP"
     sleep 5
     exit
 fi
@@ -26,24 +26,46 @@ read SUBNET_ID
 
 NAT_GATEWAY_ID=$(aws ec2 create-nat-gateway \
     --subnet-id $SUBNET_ID \
-    --allocation-id $ALLOCATION_ID \    # Associates created EIP (ALLOCATION_ID) with NAT gateway
+    --allocation-id $ALLOCATION_ID \
     --output text --query NatGateway.NatGatewayId)
 
-sleep 25
-
-aws ec2 describe-nat-gateways \
-    --nat-gateway-ids $NAT_GATEWAY_ID \
-    --output text --query NatGateways[0].State
-    
-echo -n "Creating route for default route (0.0.0.0/0) with destination to NAT Gateway:"
-echo -n "Loading..."
+echo "loading....."
+sleep 4
+echo "loading...."
+sleep 3
+echo "loading..."
+sleep 2
+echo "loading.."
+sleep 1
+echo "loading."
 sleep 10
+echo  "..."
+sleep 20
+echo -n "Nat Gateway status: "
+
+//aws ec2 describe-nat-gateways \
+  //                 --nat-gateway-ids $NAT_GATEWAY_ID \
+    //               --output text --query NatGateways[0].State
+
+wait $!
+
+
+echo "Creating route for default route (0.0.0.0/0) with destination to NAT Gateway:"
+echo "Loading..."
+echo "Loading...."
+echo "Loading....."
+echo "Loading......"
+sleep 12
 
 echo -n "Enter valid private rtb-id: "
-read $RTB_ID
+read RTB_ID
 aws ec2 create-route --route-table-id $RTB_ID \
     --destination-cidr-block 0.0.0.0/0 \
     --nat-gateway-id $NAT_GATEWAY_ID
 
-
-
+if [ $? != 0 ]
+then
+echo  "Route already exists in route table"
+else
+echo  "Successfully added route to route table"
+fi
