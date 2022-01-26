8ff4451460e0e18b2b2a73f8f330aea92ef344b7
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-25 20:36:08 -0500

    Update create_NAT_EIP_Route.sh

--- create_NAT_EIP_Route.sh
+++ create_NAT_EIP_Route.sh
@@ -3,18 +3,19 @@
 # Creates an Elastic IP, NAT Gateway, and route in existing route tables.
 
 echo "Create EIP to be used with NAT gateway: "
+sleep 10
 ALLOCATION_ID=$(aws ec2 allocate-address --domain vpc \
                 -output text --query AllocationId)
                 
-sleep 10
+sleep 5
                 
 if [ $? == 0 ]
 then
     echo -n "Successfully created EIP"
-    sleep 10
+    sleep 5
 else
     echo -n "Could not create EIP"
-    sleep 10
+    sleep 5
     exit
 fi
 
@@ -27,4 +28,11 @@ NAT_GATEWAY_ID=$(aws ec2 create-nat-gateway \
     --subnet-id $SUBNET_ID \
     --allocation-id $ALLOCATION_ID \    # Associates created EIP (ALLOCATION_ID) with NAT gateway
     --output text --query NatGateway.NatGatewayId)
+
+sleep 25
+aws ec2 describe-nat-gateways \
+    --nat-gateway-ids $NAT_GATEWAY_ID \
+    --output text --query NatGateways[0].State
+    
+ 
     
