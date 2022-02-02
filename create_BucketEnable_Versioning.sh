f1e79c970e4ce1d362de5ebe9fa8d0e755a81a7f
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-02 12:05:48 -0500

    Test it out,before final

--- create_BucketEnable_Versioning.sh
+++ create_BucketEnable_Versioning.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo -n "give your bucket a name: "
+read BUCKET_NAME
+aws s3api create-bucket --bucket $BUCKET_NAME
+
+echo -n "enabling versioning..."
+
+aws s3api put-bucket-versioning \
+    --bucket $BUCKET_NAME \
+    --versioning-configuration Status=Enabled
+    
+   
 
 
-aws s3api create-bucket --bucket 
